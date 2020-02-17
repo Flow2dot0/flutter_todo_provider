0d456f6818650668e310c5369ee29988fef08ce0
Flow2dot0 <florian.gustin@edu.itescia.fr>
2020-02-17 02:59:35 +0100

    style : tries on custom clipper, finally adding the pkg

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MaterialApp(
-      home: TaskScreen(),
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: TaskScreen(),
+      ),
+    );
 
 class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 40.0, right: 35.0),
+            width: MediaQuery.of(context).size.width,
+            height: 200.0,
+            color: Colors.lightBlueAccent,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  'My Todo App',
+                  style: GoogleFonts.pacifico(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 45.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Text(
+                  '14 tasks',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ClipPath(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: Text('Test'),
+              ),
+              clipper: WaveClipperTwo(reverse: true),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
